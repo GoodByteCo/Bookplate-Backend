7c28bb59183218b35b512f4cf780809dd0fe03af
Quinn Pollock <contact@quinnpollock.net>
2020-03-20 15:07:32 -0400

    Change Callback urls

--- routes/auth/auth.go
+++ routes/auth/auth.go
@@ -12,12 +12,14 @@ import (
 	"github.com/markbates/goth/providers/google"
 )
 
+
+var h = sha1.New()
+
 func init() {
-	key := []byte(os.Getenv("SESSION_SECRET"))
-	fmt.Println(key)
+	//gothic.Store :=
 	goth.UseProviders(
-		google.New(os.Getenv("GOOGLE_CLIENT"), os.Getenv("GOOGLE_SECRET"), "http://localhost:8080/auth/callback?provider=google"),
-		github.New(os.Getenv("GITHUB_CLIENT"), os.Getenv("GITHUB_SECRET"), "http://localhost:8080/auth/callback?provider=github"),
+		google.New(os.Getenv("GOOGLE_CLIENT"), os.Getenv("GOOGLE_SECRET"), "http://localhost:8081/api/auth/callback?provider=google"),
+		github.New(os.Getenv("GITHUB_CLIENT"), os.Getenv("GITHUB_SECRET"), "http://localhost:8081/api/auth/callback?provider=github"),
 	)
 }
 
